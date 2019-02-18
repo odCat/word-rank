97d4ade1b2393caac58438639460f1f38a272340
Mihai Gătejescu <gus666xe@gmail.com>
2019-02-18 23:25:00 +0200

    Count the characters first

--- rank.c
+++ rank.c
@@ -6,45 +6,59 @@ FILE * sample;
 bool eof_flag = false;
 
 /* prototypes */
+void open_file();
+long int count_char(FILE *);
 char * read_words(void);
 
 int main (void)
 {
-    sample = fopen ("sample.txt", "r");
-
-    char *string;
-
-    while (!eof_flag)
-    {
-        printf ("%s ", read_words());
-    }
-
-    fclose (sample);
+    open_file();
 
     return 0;
 }
 
-char * read_words(void)
+void open_file()
 {
-    int     ch;
-    char    temp;
-    char    *word, *word_temp;
-    bool    in_word = false;
+    sample = fopen ("sample.txt", "r");
 
-    word_temp = word = &temp;
-    while ((ch = getc(sample)) != EOF)
-    {
-        if (isalpha(ch))
-        {
-            if (!in_word)
-                in_word = true;
-            *word_temp = ch;
-            word_temp++;
-        } else
-            in_word = false;
-    }
-    *word_temp = '\0';
-    eof_flag = true;
+    printf ("%ld\n", count_char(sample));
 
-    return word;
+    fclose (sample);
 }
+
+long int count_char(FILE *f)
+{
+    long count;
+
+    while (getc(f) != EOF)
+    {
+        ++count;
+    }
+
+    return count;
+}
+
+/* char * read_words(void) */
+/* { */
+/*     int     ch; */
+/*     char    temp; */
+/*     char    *word, *word_temp; */
+/*     bool    in_word = false; */
+
+/*     word_temp = word = &temp; */
+/*     while ((ch = getc(sample)) != EOF) */
+/*     { */
+/*         if (isalpha(ch)) */
+/*         { */
+/*             if (!in_word) */
+/*                 in_word = true; */
+/*             *word_temp = ch; */
+/*             word_temp++; */
+/*         } else */
+/*             in_word = false; */
+/*     } */
+/*     *word_temp = '\0'; */
+/*     eof_flag = true; */
+
+/*     return word; */
+/* } */
